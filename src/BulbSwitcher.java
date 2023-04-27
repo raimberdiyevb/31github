7a9b9dec9d34c6973c31b37ae71078cf60bee1d1
Bekbolsun Raimberdiyev <bekbolsun.raimberdiev@gmail.com>
2023-04-27 18:33:23 +0200

    Solved a question.

---
+++ src/BulbSwitcher.java
@@ -0,0 +1,9 @@
+public class BulbSwitcher {
+    public static void main(String[] args) {
+        System.out.println(bulbSwitch(3));
+
+    }
+    public static int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
