f08beea663b3f87edd5cd32140b2c50840d7abf4
Bekbolsun Raimberdiyev <bekbolsun.raimberdiev@gmail.com>
2023-11-17 08:29:27 +0100

    Solved a problem.

--- src/SortArrayByNumberOf1byte.java
+++ src/SortArrayByNumberOf1byte.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class SortArrayByNumberOf1byte {
+import java.util.*;
+
+public class SortArrayByNumberOf1byte {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sort(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8})));
+    }
+    public static int[] sort(int[] arr){
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int[] ones = new int[arr.length];
+        for (int i : arr){
+            int count = Integer.bitCount(i);
+            map.putIfAbsent(count, new ArrayList<>());
+            map.get(count).add(i);
+        }
+        List<Integer> res = new ArrayList<>();
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        for(int i : keys){
+            List<Integer> values = map.get(i);
+            res.addAll(values);
+        }
+        int k = 0;
+        for (int i : res) {
+            ones[k] = i;
+            k++;
+        }
+        return ones;
+    }
 }
