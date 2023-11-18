c12efb202a59f271b6898c1f7e9c752de65601e8
Bekbolsun Raimberdiyev <bekbolsun.raimberdiev@gmail.com>
2023-11-18 09:40:22 +0100

    Solved a problem.

--- src/CountNodesEqualToAverageOfSubtree.java
+++ src/CountNodesEqualToAverageOfSubtree.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class CountNodesEqualToAverageOfSubtree {
+public class CountNodesEqualToAverageOfSubtree {
+    public static void main(String[] args) {
+
+    }
+//    public static int averageOfSubtree(TreeNode root) {
+//        // for each node
+//            // use get average
+//            // if n == avg int++
+//        // return int
+//        Queue<Integer> q = new LinkedList<>();
+//        q.offer(root1)
+//    }
+
+
 }
