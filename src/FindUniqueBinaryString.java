8526b0c9a0b8c2db98bcea7cf5db74c73752c177
Bekbolsun Raimberdiyev <bekbolsun.raimberdiev@gmail.com>
2023-11-19 10:48:01 +0100

    Solved a problem.

--- src/FindUniqueBinaryString.java
+++ src/FindUniqueBinaryString.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class FindUniqueBinaryString {
+public class FindUniqueBinaryString {
+    public static void main(String[] args) {
+        System.out.println(findDifferentBinaryString(new String[]{"111","011","001"}));
+    }
+    public static String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i].charAt(i)=='0') sb.append('1');
+            else sb.append('0');
+        }
+        return sb.toString();
+    }
 }
